44357823d3efac2f74ffe7b8b83cea3e8ac1adca
bilal Patiwala <bilalpatiwala04@gmail.com>
2022-08-12 18:43:05 +0530

    added enviormental variable

--- Class101/settings.py
+++ Class101/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,12 @@ AUTH_USER_MODEL = 'account.User'
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*hev$*5-u-1f(xyel4ohq*16jytx3c+2i(@mcp-c#@4&6izui^'
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+SECRET_KEY = os.environ['SECRET_KEY']
+print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
