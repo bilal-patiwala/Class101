377917e596649901aa685f0f750053cd833c714c
bilal Patiwala <bilalpatiwala04@gmail.com>
2022-08-14 15:31:55 +0530

    edi profile image updated

--- TeacherPortal/views.py
+++ TeacherPortal/views.py
@@ -10,22 +10,29 @@ def teacherDetail(request):
     teacher = Teacher.objects.get(user=request.user)
     teacher_name = request.user.username
     if request.method == 'POST':
-        class_name = request.POST['class_name']
-        section = request.POST['section']
-        subject_name = request.POST['subject_name']
+        if request.POST.get('create_class'):
+            class_name = request.POST['class_name']
+            section = request.POST['section']
+            subject_name = request.POST['subject_name']
 
-        if class_name == "":
-            messages.warning(request,'class name is empty ')
+            if class_name == "":
+                messages.warning(request,'class name is empty ')
+                return redirect('teacher_portal:teacher_detail')
+
+            new_class = CreateClass.objects.create(teacher=teacher,class_name=class_name,section=section,subject_name=subject_name)
+            new_class.save()
             return redirect('teacher_portal:teacher_detail')
 
-        new_class = CreateClass.objects.create(teacher=teacher,class_name=class_name,section=section,subject_name=subject_name)
-        new_class.save()
-        return redirect('teacher_portal:teacher_detail')
+        if request.POST.get('edit_image'):
+            teacher.teacher_profile_pic = request.FILES['edit_profile']
+            teacher.save()
+            return redirect('teacher_portal:teacher_detail')
     
     
 
     classes = reversed(CreateClass.objects.filter(teacher=teacher))
     context = {
+        'teacher':teacher,
         'teacher_name':teacher_name,
         'classes':classes
     }
