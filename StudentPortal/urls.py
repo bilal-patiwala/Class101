dc22aa8e946cc118b3b86094da3141738b58182e
bilal Patiwala <bilalpatiwala04@gmail.com>
2022-08-22 22:20:18 +0530

    student detail page added

--- StudentPortal/urls.py
+++ StudentPortal/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 app_name = "student_portal"
 urlpatterns = [
-    path('student_detail',views.studentDetail,name="student_detail")
+    path('student_detail',views.studentDetail,name="student_detail"),
+    path('logout',views.logout,name="logout")
 ]
--- StudentPortal/views.py
+++ StudentPortal/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import auth
+from TeacherPortal.models import CreateClass
+from account.models import Student
+
 
 # Create your views here.
 def studentDetail(request):
-    return render(request,'StudentPortal/student_detail_page.html')
+    student = Student.objects.get(user=request.user)
+    classes = reversed(CreateClass.objects.filter(student=student))
+    context = {
+        'classes':classes,
+    }
+    return render(request,'StudentPortal/student_detail_page.html',context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect('account:welcome') 
